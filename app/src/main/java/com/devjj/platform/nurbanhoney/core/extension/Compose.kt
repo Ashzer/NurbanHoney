24957c0cccf35e532a7ddea6f871b5973882895c
ashzer <inty.coffee@gmail.com>
2023-07-06 18:57:55 +0900

    Fixed datastore

--- app/src/main/java/com/devjj/platform/nurbanhoney/core/extension/Compose.kt
+++ app/src/main/java/com/devjj/platform/nurbanhoney/core/extension/Compose.kt
@@ -1,14 +1,30 @@
 package com.devjj.platform.nurbanhoney.core.extension
 
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.LayoutDirection
+
+@Composable
+fun alignRight(content: @Composable () -> Unit): @Composable() (ColumnScope.() -> Unit)? = @Composable{ ->
+    CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Rtl) { content() }
+}
+
+
+@Composable
+fun AlignLeft(content: @Composable () -> Unit) =
+    CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Ltr) { content() }
+
 
 @Composable
 fun AlignRight(content: @Composable () -> Unit) =
     CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Rtl) { content() }
 
-@Composable
-fun AlignLeft(content: @Composable () -> Unit) =
-    CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Ltr) { content() }
+
--- app/src/main/java/com/devjj/platform/nurbanhoney/feature/ui/home/HomePage.kt
+++ app/src/main/java/com/devjj/platform/nurbanhoney/feature/ui/home/HomePage.kt
@@ -9,17 +9,16 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.devjj.platform.nurbanhoney.R
-import com.devjj.platform.nurbanhoney.core.extension.AlignLeft
-import com.devjj.platform.nurbanhoney.core.extension.AlignRight
+import com.devjj.platform.nurbanhoney.core.extension.alignRight
 import com.devjj.platform.nurbanhoney.core.platform.*
 import com.devjj.platform.nurbanhoney.feature.ui.BoardsTab
 import kotlinx.coroutines.CoroutineScope
@@ -38,104 +37,98 @@ fun HomePage(
     val context = LocalContext.current.applicationContext
     val drawerState = scaffoldState.drawerState
 
-    AlignRight {
-        MaterialTheme {
-            Scaffold(
-                scaffoldState = scaffoldState,
-                backgroundColor = Color.Transparent,
 
-                // Align left
-                topBar = {
-                    MainTopBar(state) {
-                        onTabSelected(it)
-                    }
-                },
-                bottomBar = {
-                    AlignLeft {
-                        DrawBottomNavigation(
-                            navController
-                        )
-                    }
-                },
-                content = { paddingValues ->
-                    AlignLeft {
-                        // MainDrawer(scaffoldState.drawerState)
-                        Column {
-                            LazyRow() {
-                                items(500) {
-                                    Card(
-                                        modifier = Modifier
-											.padding(5.dp)
-											.width(200.dp)
-											.height(120.dp),
-                                        backgroundColor = Color(0x33F6B748),
-                                        shape = RoundedCornerShape(30.dp)
-                                    ) {}
-                                }
-                            }
+    MaterialTheme {
+        Scaffold(
+            scaffoldState = scaffoldState,
+            backgroundColor = Color.Transparent,
 
-                            Box(
+            // Align left
+            topBar = {
+                MainTopBar(state) {
+                    onTabSelected(it)
+                }
+            },
+            bottomBar = {
+                DrawBottomNavigation(
+                    navController
+                )
+
+            },
+            content = { paddingValues ->
+                // MainDrawer(scaffoldState.drawerState)
+                Column {
+                    LazyRow() {
+                        items(500) {
+                            Card(
                                 modifier = Modifier
-									.fillMaxSize()
-									.padding(paddingValues)
-                            ) {
-                                LazyColumn(content = {
-                                    state.articlePreviews?.let { previews ->
-                                        items(previews.size) {
-                                            ArticlePreview(previews[it]) { index ->
-                                                onArticleClicked(index)
-                                            }
-                                        }
-                                    }
-                                })
+                                    .padding(5.dp)
+                                    .width(200.dp)
+                                    .height(120.dp),
+                                backgroundColor = Color(0x33F6B748),
+                                shape = RoundedCornerShape(30.dp)
+                            ) {}
+                        }
+                    }
 
-                                when (state.state) {
-                                    is HomeUiState.ArticlePreviewsLoading -> {
-                                        LoadingIndicator(modifier = Modifier.fillMaxSize())
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(paddingValues)
+                    ) {
+                        LazyColumn(content = {
+                            state.articlePreviews?.let { previews ->
+                                items(previews.size) {
+                                    ArticlePreview(previews[it]) { index ->
+                                        onArticleClicked(index)
                                     }
-                                    is HomeUiState.Failed -> {
-                                        ErrorMessage(
-                                            message = state.state.message,
-                                            modifier = Modifier.fillMaxSize()
-                                        )
-                                    }
-                                    else -> Unit
                                 }
                             }
+                        })
 
-                        }
-                        paddingValues.calculateTopPadding()
-                    }
-                },
-                drawerContent = {
-                    AlignLeft {
-                        DrawerContent { itemLabel ->
-                            Toast.makeText(
-                                context,
-                                itemLabel,
-                                Toast.LENGTH_SHORT
-                            ).show()
-                            coroutineScope.launch {
-                                // delay for the ripple effect
-                                delay(timeMillis = 250)
-                                drawerState.close()
+                        when (state.state) {
+                            is HomeUiState.ArticlePreviewsLoading -> {
+                                LoadingIndicator(modifier = Modifier.fillMaxSize())
                             }
+                            is HomeUiState.Failed -> {
+                                ErrorMessage(
+                                    message = state.state.message,
+                                    modifier = Modifier.fillMaxSize()
+                                )
+                            }
+                            else -> Unit
                         }
                     }
-                },
-                floatingActionButton = {
-					AlignLeft{
-						FloatingActionButton(onClick = { /*TODO*/ }) {
-							Image(
-								painter = painterResource(id = R.drawable.write),
-								contentDescription = null
-							)
-						}
 
-					}
-				}
-            )
-        }
+                }
+                paddingValues.calculateTopPadding()
+
+            },
+            drawerContent = alignRight{
+                    DrawerContent { itemLabel ->
+                        Toast.makeText(
+                            context,
+                            itemLabel,
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        coroutineScope.launch {
+                            // delay for the ripple effect
+                            delay(timeMillis = 250)
+                            drawerState.close()
+                        }
+                    }
+
+            },
+            floatingActionButton = {
+                FloatingActionButton(onClick = { /*TODO*/ }) {
+                    Image(
+                        painter = painterResource(id = R.drawable.write),
+                        contentDescription = null
+                    )
+                }
+            }
+        )
+
     }
 }
 
@@ -157,15 +150,12 @@ fun MainTopBar(
     }
 
     Column {
-        AlignLeft {
-            MainToolBar(
-                scaffoldState.drawerState,
-                coroutineScope
-            )
-        }
-        AlignLeft {
-            BoardsTab(state) { onTabSelected(it) }
-        }
+        MainToolBar(
+            scaffoldState.drawerState,
+            coroutineScope
+        )
+        BoardsTab(state) { onTabSelected(it) }
+
     }
 }
 
@@ -178,44 +168,34 @@ fun BasePage(
     context: Context,
     content: @Composable () -> Unit
 ) {
-    AlignRight {
-        MaterialTheme {
-            Scaffold(
-                scaffoldState = scaffoldState,
-                backgroundColor = Color.Blue,
+    MaterialTheme {
+        Scaffold(
+            scaffoldState = scaffoldState,
+            backgroundColor = Color.Blue,
 
-                // Align left
-                topBar = {
-                    AlignLeft {
-                        MainToolBar(scaffoldState.drawerState, coroutineScope)
-                    }
-                },
-                bottomBar = {
-                    AlignLeft {
-                        DrawBottomNavigation(navController)
-                    }
-                },
-                content = {
-                    it
-                    AlignLeft {
+            // Align left
+            topBar = {
+                MainToolBar(scaffoldState.drawerState, coroutineScope)
+            },
+            bottomBar = {
+                DrawBottomNavigation(navController)
 
-                    }
-                },
-                drawerContent = {
-                    AlignLeft {
-                        DrawerContent { itemLabel ->
-                            Toast
-                                .makeText(context, itemLabel, Toast.LENGTH_SHORT)
-                                .show()
-                            coroutineScope.launch {
-                                // delay for the ripple effect
-                                delay(timeMillis = 250)
-                                drawerState.close()
-                            }
-                        }
+            },
+            content = {
+                it
+            },
+            drawerContent = {
+                DrawerContent { itemLabel ->
+                    Toast
+                        .makeText(context, itemLabel, Toast.LENGTH_SHORT)
+                        .show()
+                    coroutineScope.launch {
+                        // delay for the ripple effect
+                        delay(timeMillis = 250)
+                        drawerState.close()
                     }
                 }
-            )
-        }
+            }
+        )
     }
 }
